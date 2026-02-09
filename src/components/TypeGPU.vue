2547989ed88b7297bb73ef83ae5c4da95145143f
plastic041 <plastic041@gmail.com>
2026-02-09 13:21:40 +0900

    shader

--- src/components/TypeGPU.vue
+++ src/components/TypeGPU.vue
@@ -9,6 +9,7 @@ import tgpu, {
 } from "typegpu";
 import * as d from "typegpu/data";
 import { onMounted, onUnmounted } from "vue";
+import { shaderCode } from "../shaders/image.ts";
 
 const Adjustments = d.struct({
   exposure: d.f32,
@@ -50,61 +51,6 @@ async function ready() {
 
   adjustmentsBuffer = root.createBuffer(Adjustments).$usage("uniform");
 }
-
-const shaderCode = /* wgsl */ `
-@vertex
-fn main_vert(@builtin(vertex_index) index: u32) -> VertexOutput {
-  const vertices = array<vec2f, 4>(
-    vec2f(-1.0, -1.0), // Bottom-left
-    vec2f(-1.0,  1.0), // Top-left
-    vec2f( 1.0, -1.0), // Bottom-right
-    vec2f( 1.0,  1.0)  // Top-right
-  );
-
-  let pos = vertices[index];
-  var output: VertexOutput;
-  output.position = vec4f(pos, 0.0, 1.0);
-
-  output.uv = vec2f((pos.x + 1.0) * 0.5, 1.0 - (pos.y + 1.0) * 0.5);
-  return output;
-}
-
-@fragment
-fn main_frag(@location(0) uv: vec2f) -> @location(0) vec4f {
-  let color = textureSample(inTexture, inSampler, uv).rgb;
-  let inputLuminance = dot(color, vec3f(0.299, 0.587, 0.114));
-  let normColor = clamp(color, vec3f(0.0), vec3f(1.0));
-
-  let exposureBiased = adjustments.exposure * 0.25;
-  let exposureColor = clamp(normColor * pow(2.0, exposureBiased), vec3f(0.0), vec3f(2.0));
-  let exposureLuminance = clamp(inputLuminance * pow(2.0, exposureBiased), 0.0, 2.0);
-
-  let contrastColor = (exposureColor - vec3f(0.5)) * adjustments.contrast + vec3f(0.5);
-  let contrastLuminance = (exposureLuminance - 0.5) * adjustments.contrast + 0.5;
-  let contrastColorLuminance = dot(contrastColor, vec3f(0.299, 0.587, 0.114));
-
-  let highlightShift = adjustments.highlights - 1.0;
-  let highlightBiased = select(highlightShift * 0.25, highlightShift, adjustments.highlights >= 1.0);
-  let highlightFactor = 1.0 + highlightBiased * 0.5 * contrastColorLuminance;
-  let highlightWeight = smoothstep(0.5, 1.0, contrastColorLuminance);
-  let highlightLuminanceAdjust = contrastLuminance * highlightFactor;
-  let highlightLuminance = mix(contrastLuminance, clamp(highlightLuminanceAdjust, 0.0, 1.0), highlightWeight);
-  let highlightColor = mix(contrastColor, clamp(contrastColor * highlightFactor, vec3f(0.0), vec3f(1.0)), highlightWeight);
-
-  let shadowWeight = 1.0 - contrastColorLuminance;
-  let shadowAdjust = pow(highlightColor, vec3f(1.0 / adjustments.shadows));
-  let shadowLuminanceAdjust = pow(highlightLuminance, 1.0 / adjustments.shadows);
-
-  let toneColor = mix(highlightColor, shadowAdjust, shadowWeight);
-  let toneLuminance = mix(highlightLuminance, shadowLuminanceAdjust, shadowWeight);
-
-  let finalToneColor = clamp(toneColor, vec3f(0.0), vec3f(1.0));
-  let grayscaleColor = vec3f(toneLuminance);
-  let finalColor = mix(grayscaleColor, finalToneColor, adjustments.saturation);
-
-  return vec4f(finalColor, 1.0);
-}
-`;
 
 function render() {
   const uniformLayout = tgpu.bindGroupLayout({
@@ -201,70 +147,22 @@ async function init() {
   render();
 }
 
-// const controls = {
-//   exposure: {
-//     initial: 0.0,
-//     min: -2.0,
-//     max: 2.0,
-//     step: 0.1,
-//     onSliderChange(value: number) {
-//       adjustmentsBuffer.writePartial({ exposure: value });
-//       loop();
-//     },
-//   },
-//   contrast: {
-//     initial: 1.0,
-//     min: 0.0,
-//     max: 2.0,
-//     step: 0.1,
-//     onSliderChange(value: number) {
-//       adjustmentsBuffer.writePartial({ contrast: value });
-//       loop();
-//     },
-//   },
-//   highlights: {
-//     initial: 1.0,
-//     min: 0.0,
-//     max: 2.0,
-//     step: 0.1,
-//     onSliderChange(value: number) {
-//       adjustmentsBuffer.writePartial({ highlights: value });
-//       loop();
-//     },
-//   },
-//   shadows: {
-//     initial: 1.0,
-//     min: 0.1,
-//     max: 1.9,
-//     step: 0.1,
-//     onSliderChange(value: number) {
-//       adjustmentsBuffer.writePartial({ shadows: value });
-//       loop();
-//     },
-//   },
-//   saturation: {
-//     initial: 1.0,
-//     min: 0.0,
-//     max: 2.0,
-//     step: 0.1,
-//     onSliderChange(value: number) {
-//       adjustmentsBuffer.writePartial({ saturation: value });
-//       loop();
-//     },
-//   },
-// };
-
 function onCleanup() {
   root.destroy();
 }
 
+function setBufferValue(key: keyof typeof adjustmentsBuffer.dataType.propTypes, value: number) {
+  adjustmentsBuffer.writePartial({ [key]: value });
+  render();
+}
+
 onMounted(async () => {
   await ready();
-  adjustmentsBuffer.writePartial({ contrast: 1 });
   adjustmentsBuffer.writePartial({ exposure: 1 });
   adjustmentsBuffer.writePartial({ highlights: 1 });
   adjustmentsBuffer.writePartial({ saturation: 1 });
   adjustmentsBuffer.writePartial({ shadows: 1 });
+  adjustmentsBuffer.writePartial({ contrast: 1 });
   init();
 });
 
@@ -275,4 +173,81 @@ onUnmounted(() => {
 
 <template>
   <canvas />
+  <div class="flex flex-col">
+    <label>
+      Exposure
+      <input
+        type="range"
+        min="-2"
+        max="2"
+        step="0.1"
+        defaultValue="1"
+        @input="
+          (e) => {
+            setBufferValue('exposure', (e.currentTarget as HTMLInputElement).valueAsNumber);
+          }
+        "
+      />
+    </label>
+    <label>
+      Highlights
+      <input
+        type="range"
+        min="0"
+        max="2"
+        step="0.1"
+        defaultValue="1"
+        @input="
+          (e) => {
+            setBufferValue('highlights', (e.currentTarget as HTMLInputElement).valueAsNumber);
+          }
+        "
+      />
+    </label>
+    <label>
+      Saturation
+      <input
+        type="range"
+        min="0"
+        max="2"
+        step="0.1"
+        defaultValue="1"
+        @input="
+          (e) => {
+            setBufferValue('saturation', (e.currentTarget as HTMLInputElement).valueAsNumber);
+          }
+        "
+      />
+    </label>
+    <label>
+      Shadows
+      <input
+        type="range"
+        min="0.1"
+        max="1.9"
+        step="0.1"
+        defaultValue="1"
+        @input="
+          (e) => {
+            setBufferValue('shadows', (e.currentTarget as HTMLInputElement).valueAsNumber);
+          }
+        "
+      />
+    </label>
+    <label>
+      Contrast
+      <input
+        type="range"
+        min="0"
+        max="2"
+        step="0.1"
+        defaultValue="1"
+        @input="
+          (e) => {
+            setBufferValue('contrast', (e.currentTarget as HTMLInputElement).valueAsNumber);
+          }
+        "
+      />
+    </label>
+  </div>
 </template>
